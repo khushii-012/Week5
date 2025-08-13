35f06093c74de7c1f261f4713a1c33e32843ff4c
khushii-012 <khushilanjewar220@gmail.com>
2025-08-14 00:34:47 +0530

    Added Week 5 JavaScript tasks

---
+++ script.js
@@ -0,0 +1,35 @@
+// let button = document.getElementById("button");
+// let para =document.getElementById("para");
+
+//         button.addEventListener('click', () => {
+//             console.log("button was clicked!!");
+//         });
+
+// Task2
+// let btn = document.getElementById("button");
+// btn.addEventListener("click",()=>{
+//     alert("Welcome to the Week5");
+//  });
+
+
+
+
+//Task3
+ let button = document.getElementById("button");
+ button.addEventListener("click", () => {
+            // Pick a random color
+            const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+            document.body.style.backgroundColor = randomColor;
+            });
+
+//Task6
+// let button = document.getElementById("button");
+//  console.log("start");
+
+//  setTimeout(()=> {
+//      for(let i=0;i<=1000;i++){
+//          console.log("middle");
+//     }
+// },0);
+
+//  console.log("end");
